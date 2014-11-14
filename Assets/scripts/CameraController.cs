53535d19b599bd6c4d6da44cb7f091b4b2d5ff23
suzuki_yoshifumi <suzuki_yoshifumi@cyberagent.co.jp>
2014-11-14 12:10:18 +0900

    ステージと経路探索

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+//	private Vector3 _initPos;
+//	private Quaternion _initRot;
+
+//	private GameObject _floor;
+//	public float _radius = 15.0f;	// オブジェクトからカメラまでの距離(円運動の半径)
+//	public float _angle = 0.0f;	// ラジアン値
+
+	// Use this for initialization
+	void Start () {
+//		_initPos = transform.position;
+//		_initRot = transform.rotation;
+//		_floor = GameObject.Find("Floor");
+
+//		Vector3 pos = _floor.transform.position;
+//		transform.LookAt(pos);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Vector3 offset = new Vector3 ( Mathf.Sin(Time.time), 0, 0 );
+//		transform.Translate( new Vector3(_initPos.x + offset.x, 0, 0) );
+//		Vector3 pos = _floor.transform.position;
+//		transform.LookAt(pos);
+//
+//		transform.position = new Vector3(Mathf.Cos(_angle) * 10, _initPos.y, Mathf.Sin(_angle) * 10);
+//		_angle += 0.005f;
+
+		transform.Rotate(0, 0.2f, 0);
+	}
+}
+ 
---
+++ Assets/scripts/ZombieController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+	private GameObject _body;
+	private GameObject _goal;
+	private NavMeshAgent _agent;
+	private ArrayList _animList;
+
+	// Use this for initialization
+	void Start () {
+		_body = gameObject.transform.FindChild("ZombieBody").gameObject; 
+		_animList = new ArrayList();
+		foreach (AnimationState anim in _body.animation) {
+			Debug.Log(anim.name);
+			_animList.Add(anim);
+		}
+
+//		_animList = _body.GetComponent("Animations") as Animation[];
+//		_animList = _body. 
+		_goal = GameObject.Find("Goal");
+		_agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_agent.SetDestination(_goal.transform.position);
+	}
+}
