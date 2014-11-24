0f450a5349f2565c2519892dd5853b782c5545d9
kurooooon <b1006110@gmail.com>
2014-11-25 03:39:42 +0900

    add human

---
+++ Assets/scripts/HumanController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanController : MonoBehaviour {
+
+	private GameObject _body;
+	private GameObject _goal;
+	private NavMeshAgent _agent;
+	private ArrayList _animList; 
+	private bool _isAlive;
+	
+	// Use this for initialization
+	void Start () {
+		_isAlive = true;
+		_body = gameObject.transform.FindChild("MAX").gameObject; 
+		_animList = new ArrayList(); 
+		foreach (AnimationState anim in _body.animation) {
+			//			Debug.Log(anim.name);
+			_animList.Add(anim);
+		}
+		
+		//		_animList = _body.GetComponent("Animations") as Animation[];
+		//		_animList = _body. 
+		_goal = GameObject.Find("Goal");
+		_agent = GetComponent<NavMeshAgent>();
+		
+		_body.animation.wrapMode = WrapMode.Loop;
+		_body.animation.Play("run");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_isAlive)
+			_agent.SetDestination(_goal.transform.position);
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		if ( col.gameObject.tag.IndexOf(GameConfig.BULLET_CODE) >= 0 && _isAlive )
+		{
+			_isAlive = false;
+			_agent.Stop();
+			Debug.Log("fuck!!");
+			_body.animation.wrapMode = WrapMode.Default;
+			
+			StartCoroutine("death");
+		}
+	}
+	
+	private IEnumerator death() {
+		
+		_body.animation.Play("death");
+		
+		yield return new WaitForSeconds (1.8f);
+		
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/scripts/HumanFactory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanFactory : MonoBehaviour {
+
+	public GameObject humanPF;
+	
+	private GameObject _factory;
+	
+	// Use this for initialization
+	void Start () {
+		_factory = transform.FindChild("InitPos").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rand = Random.value;
+		if (rand > 0.99f)
+		{
+			Debug.Log("generate human");
+			float num = Random.Range(1, 3);
+			for (int i = 0; i < num; i++)
+			{
+				GameObject human = Instantiate(humanPF, _factory.transform.position, transform.rotation) as GameObject;
+			}
+		}
+	}
+}
